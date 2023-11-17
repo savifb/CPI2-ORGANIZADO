615d92fad20f6135dc71b0cd3cff2a0c5517eacc
savifb <129786165+savifb@users.noreply.github.com>
2023-11-17 18:47:00 -0300

    Revisão - 2

---
+++ arquivos-iniciais/js/produtos.js
@@ -0,0 +1,37 @@
+const produtos = [
+					{
+						id: 1,
+						nome: 'banana',
+						preco: 4.99
+					},
+					{
+						id: 2,
+						nome: 'abacate',
+						preco: 9.99
+					},
+					{
+						id: 3,
+						nome: "laranja lima",
+						preco: 6.99
+					},
+					{
+						id: 4,
+						nome: "laranja pera",
+						preco: 2.99
+					},
+					{
+						id: 5,
+						nome: "abacaxi",
+						preco: 7.99
+					},
+					{
+						id: 6,
+						nome: "chuchu",
+						preco: 2.99
+					},
+					{
+						id: 7,
+						nome: "batata",
+						preco: 3.99
+					}
+				];
