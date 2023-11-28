b6eae7c2080c411d0ba01ed18f77b5d2b1fa454a
Savio <savio.sousa@estudante.ifb.edu.br>
2023-11-28 20:15:58 -0300

    aula 16-11 - feita

--- CPI2/16-11/IntroVue-ArquivosIniciais/main.js
+++ CPI2/16-11/IntroVue-ArquivosIniciais/main.js
@@ -1,1 +1,11 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            produto: 'Camiseta Próton',
+            descricao: 'Uma camiseta de prótons, linda, pronta para entrega'
+        }
+    }
+});
+
+const montApp = app.mount('#app');
 
