4cf064c0b699e598106998fed2d3d8c2e8030dfe
Savio <savio.sousa@estudante.ifb.edu.br>
2023-11-30 16:47:09 -0300

    16-11 publicando

--- CPI2/16-11/IntroVue-ArquivosIniciais/main.js
+++ CPI2/16-11/IntroVue-ArquivosIniciais/main.js
@@ -3,12 +3,18 @@ const app = Vue.createApp({
         return {
             produto: 'Camiseta Próton',
             image : './assets/images/proton_branca.jpg',
+            estoque: 10,
             descricao: 'Uma camiseta de algodão com o logo da Próton',
-            link: 'https://pt.wikipedia.org/wiki/Wikip%C3%A9dia:P%C3%A1gina_principal'
-            
+            link: 'https://pt.wikipedia.org/wiki/Wikip%C3%A9dia:P%C3%A1gina_principal',
+            promocao: true,
+            detalhes: ['100% algodão', 'Cor: Branca', 'Unissex'],
+            cores: [
+                {id: 1, cor: 'Branca'},
+                {id: 2, cor: 'Preta'},
+                // adicione mais cores aqui
+            ]
         }
     }
 });
 
 const montApp = app.mount('#app');
-
